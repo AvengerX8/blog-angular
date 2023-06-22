62845cdd15678d19648afd19fe80572c8026b784
Alexsander Furtado <alexsander.f.lex@gmail.com>
2023-06-21 23:20:52 -0300

    Blog Angular

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "DARTH VADER QUER DOMÍNIO TOTAL!",
+    "description": "Darth Vader, o icônico Lorde Sith do universo Star Wars, está atualmente empenhado em uma campanha implacável para expandir seus domínios. Com sua poderosa presença e habilidades formidáveis na Força, ele tem conquistado e subjugado inúmeros sistemas planetários ao longo da galáxia. Sua determinação e crueldade infundem temor nos corações daqueles que ousam desafiá-lo, enquanto ele se esforça para consolidar seu poder e estabelecer seu reinado sombrio. À medida que Darth Vader estende seu alcance, a opressão e a escuridão seguem em seu rastro, deixando um legado sinistro por onde passa. O destino da galáxia agora pende na balança, enquanto os heróis lutam para resistir à sombra crescente do Senhor Sith e preservar a luz que resta.",
+    "photoCover":"https://th.bing.com/th/id/R.896497b030854083a62e0184256b5008?rik=Q0fB0TNcq8IVBQ&riu=http%3a%2f%2freggiestake.files.wordpress.com%2f2012%2f06%2fdarth-vader-3.jpeg&ehk=oK%2bLUbp%2fC8nEj77bFZP6wxLJxOLEy9RmLIon0mVR%2feU%3d&risl=&pid=ImgRaw&r=0"
+  },
+  {
+    "id":"2",
+    "title": "Tensão no relacionamento entre Han Solo e Léia Organa!",
+    "description": "Em uma reviravolta surpreendente na galáxia muito, muito distante, surgem novas revelações sobre o infame contrabandista Han Solo e sua relação com a nobre Princesa Leia Organa. Fontes confiáveis indicam que Han Solo teria traído a confiança da Princesa Leia, uma das líderes da Aliança Rebelde, durante uma importante missão. Rumores sugerem que Solo teria entregado informações cruciais aos agentes do Império Galáctico, comprometendo assim a segurança da Rebelião. Essa traição deixa fãs de Star Wars perplexos e se perguntando sobre as motivações ocultas do icônico herói. Aguardamos ansiosamente por mais detalhes sobre esse intrigante episódio da saga.",
+    "photoCover":"https://www.looper.com/img/gallery/strange-things-about-princess-leia-and-han-solos-relationship/their-age-gap-is-pretty-shady-1590179134.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Obi-Wan parte para uma jornada decisiva!",
+    "description": "Obi-Wan Kenobi, o lendário Cavaleiro Jedi, prepara-se para embarcar em uma jornada perigosa e emocionalmente desafiadora. Movido pela determinação e coragem que sempre o caracterizaram, ele se encaminha para enfrentar seu antigo aprendiz, o sombrio e implacável Darth Vader. Consciente da magnitude da batalha que está por vir, Obi-Wan sabe que esse confronto não se trata apenas de um embate entre dois mestres e alunos, mas sim de um choque de ideais e forças que moldará o destino da galáxia. Com seu sabre de luz nas mãos e a sabedoria dos antigos Jedi como guia, Obi-Wan Kenobi parte rumo a esse confronto épico, disposto a dar tudo de si para proteger o que resta da luz e da esperança na galáxia.",
+    "photoCover":"https://th.bing.com/th/id/OIP.rzJ21N1-T4WZMxoUs1CvkAHaE7?pid=ImgDet&rs=1"
+  },
+  {
+    "id":"4",
+    "title": "Treinamento polêmico!!!",
+    "description": "O treinamento de Luke Skywalker com o Mestre Yoda é um assunto polêmico que tem gerado debates acalorados. Alguns argumentam que foi um momento crucial na jornada do jovem Jedi, onde ele aprendeu importantes lições sobre a Força e desenvolveu suas habilidades. A relação entre Luke e Yoda trouxe à tona questões profundas sobre o autoconhecimento, a paciência e a perseverança. No entanto, há também aqueles que criticam o treinamento, alegando que foi inadequado e apressado. Eles questionam se Luke teve tempo suficiente para dominar completamente os ensinamentos Jedi e se ele estará preparado para enfrentar os desafios que virão pela frente. Apesar das divergências, não se pode negar a importância desse treinamento e o impacto que teve na evolução de Luke Skywalker.",
+    "photoCover":"https://cdn.traileraddict.com/article/wp-content/uploads/2016/09/luke-and-yoda-1024x576.jpg"
+  }
+
+]
